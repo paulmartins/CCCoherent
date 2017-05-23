8dc4422238c8a5d63bf0e117eb873567e026949c
Paul Martins <paul.p.j.martins@gmail.com>
2017-05-23 16:09:48 +0100

    add more macros

--- highlandMacros/DrawBinnings.cc
+++ highlandMacros/DrawBinnings.cc
@@ -1,8 +1,7 @@
 void DrawBinnings(){
-	bool savePlots = false; // save all plots in pdf at the address below
-	if(savePlots) std::string plotsFolder = "/data/pmartins/T2K/work/CCCoherent/Selection/Thesis_plots/DrawBinnings/";
-
-	bool useCohRW = true; // use the MC microTree where the coherent is reweighted from RS -> BS
+	// only put data sample to have correct POT normalisation
+	// should not plot the data/MC comparison at the selection level, analysis is still blind !
+	bool useCohRW = false; // use the MC microTree where the coherent is reweighted from RS -> BS
 	if (useCohRW) std::string pathToMCMicroTree = "/data/t2k/paul/MicroTrees_allsyst/RW/";
 	else std::string pathToMCMicroTree = "/data/t2k/paul/MicroTrees_allsyst/";
 	std::string pathToDataMicroTree = "/data/t2k/paul/MicroTrees_allsyst/";
@@ -11,10 +10,13 @@ void DrawBinnings(){
 	DrawingTools draw( pathToMCMicroTree+"MicroTree_MC_R3b.root" );
 	Experiment exp = SetUpExpt(pathToMCMicroTree,pathToDataMicroTree);
 
+	// phase space cuts
 	std::string true_cut = "&& truelepton_mom>200 && truelepton_mom < 5000 && truelepton_costheta >0.75 && truelepton_costheta < 1 && truepi_mom>150 && truepi_mom <1500 && truepi_costheta >0.45 && truepi_costheta < 1 && trueEmupi > 500 && trueEmupi < 6500 && trueThetaCoplanar *180/3.14 > 90";
 	std::string reco_cut = "&& selmu_mom>200 && selmu_mom < 5000 && selmu_costheta >0.75 && selmu_costheta < 1 && selpi_mom>150 && selpi_mom <1500 && selpi_costheta >0.45 && selpi_costheta < 1 && Emupi > 500 && Emupi < 6500 && ThetaCoplanar*180/3.14 > 90";
 	
+	// vector to save histos
 	TH1* histos[48];
+	
 	// FGD1 
 	TCanvas *c1 = new TCanvas("c1","FGD1",900,1000);
 
@@ -24,14 +26,12 @@ void DrawBinnings(){
 	draw.SetTitleY("Events");
 	draw.SetLegendParam(0.6,0.55,0.85,0.65);
 	double xbins[3]={0.2,0.7,5};
-	draw.Draw(exp,"all","all","selmu_mom/1000",2,xbins,"reaccoh","accum_level[][0][0]>9"+reco_cut,"text"," NOVARBIN  WCORR");
+	draw.Draw(exp,"all","all","selmu_mom/1000",2,xbins,"reaccoh","accum_level[][0][0]>9"+reco_cut,"text"," NOVARBIN NODATA  WCORR");
 	histos[0] = draw.GetLastStackTotal();
 	TLegend * leg1 = draw.GetLastLegend();
 	leg1->SetMargin(0.45);
 	leg1->SetBorderSize(1);
 	leg1->Draw();
-	if(savePlots && useCohRW) p1->SaveAs((plotsFolder+"mumom_fgd1RW.pdf").c_str());
-	else if(savePlots) p1->SaveAs((plotsFolder+"mumom_fgd1.pdf").c_str());
 	c1->cd();
 
 	TPad* p2=new TPad("p2","",0.5,0.67,1.0,1.0,0);
@@ -40,14 +40,12 @@ void DrawBinnings(){
 	draw.SetTitleY("Events");
 	draw.SetLegendParam(0.2,0.55,0.45,0.65);
 	double xbins2[3]={0.7,0.95,1};
-	draw.Draw(exp,"all","all","selmu_costheta",2,xbins2,"reaccoh","accum_level[][0][0]>9"+reco_cut,"text"," NOVARBIN  WCORR");
+	draw.Draw(exp,"all","all","selmu_costheta",2,xbins2,"reaccoh","accum_level[][0][0]>9"+reco_cut,"text"," NOVARBIN NODATA  WCORR");
 	histos[1] = draw.GetLastStackTotal();
 	TLegend * leg2 = draw.GetLastLegend();
 	leg2->SetMargin(0.45);
 	leg2->SetBorderSize(1);
 	leg2->Draw();
-	if(savePlots && useCohRW) p2->SaveAs((plotsFolder+"mutheta_fgd1RW.pdf").c_str());
-	else if(savePlots) p2->SaveAs((plotsFolder+"mutheta_fgd1.pdf").c_str());
 	c1->cd();
 
 	TPad* p3=new TPad("p3","",0.0,0.33,0.5,0.66,0);
@@ -56,14 +54,12 @@ void DrawBinnings(){
 	draw.SetTitleY("Events");
 	draw.SetLegendParam(0.6,0.55,0.85,0.65);
 	double xbins3[3]={0.15,0.4,1.5};
-	draw.Draw(exp,"all","all","selpi_mom/1000",2,xbins3,"reaccoh","accum_level[][0][0]>9 "+reco_cut,"text"," NOVARBIN  WCORR");
+	draw.Draw(exp,"all","all","selpi_mom/1000",2,xbins3,"reaccoh","accum_level[][0][0]>9 "+reco_cut,"text"," NOVARBIN NODATA  WCORR");
 	histos[2] = draw.GetLastStackTotal();
 	TLegend * leg3 = draw.GetLastLegend();
 	leg3->SetMargin(0.45);
 	leg3->SetBorderSize(1);
 	leg3->Draw();
-	if(savePlots && useCohRW) p3->SaveAs((plotsFolder+"pimom_fgd1RW.pdf").c_str());
-	else if(savePlots) p3->SaveAs((plotsFolder+"pimom_fgd1.pdf").c_str());
 	c1->cd();
 
 	TPad* p4=new TPad("p4","",0.5,0.33,1.0,0.66,0);
@@ -72,14 +68,12 @@ void DrawBinnings(){
 	draw.SetTitleY("Events");
 	draw.SetLegendParam(0.2,0.55,0.45,0.65);
 	double xbins4[3]={0.45,0.9,1};
-	draw.Draw(exp,"all","all","selpi_costheta",2,xbins4,"reaccoh","accum_level[][0][0]>9 "+reco_cut,"text"," NOVARBIN  WCORR");
+	draw.Draw(exp,"all","all","selpi_costheta",2,xbins4,"reaccoh","accum_level[][0][0]>9 "+reco_cut,"text"," NOVARBIN NODATA  WCORR");
 	histos[3] = draw.GetLastStackTotal();
 	TLegend * leg4 = draw.GetLastLegend();
 	leg4->SetMargin(0.45);
 	leg4->SetBorderSize(1);
 	leg4->Draw();
-	if(savePlots && useCohRW) p4->SaveAs((plotsFolder+"pitheta_fgd1RW.pdf").c_str());
-	else if(savePlots) p4->SaveAs((plotsFolder+"pitheta_fgd1.pdf").c_str());
 	c1->cd();
 
 	TPad* p5=new TPad("p5","",0.0,0.0,0.5,0.32,0);
@@ -88,14 +82,12 @@ void DrawBinnings(){
 	draw.SetTitleY("Events");
 	draw.SetLegendParam(0.6,0.70,0.875,0.85);
 	double xbins5[3]={0.5,1.25,6.5};
-	draw.Draw(exp,"all","all","Emupi/1000.",2,xbins5,"reaccoh","accum_level[][0][0]>9 "+reco_cut,"text"," NOVARBIN  WCORR");
+	draw.Draw(exp,"all","all","Emupi/1000.",2,xbins5,"reaccoh","accum_level[][0][0]>9 "+reco_cut,"text"," NOVARBIN NODATA  WCORR");
 	histos[4] = draw.GetLastStackTotal();
 	TLegend * leg5 = draw.GetLastLegend();
 	leg5->SetMargin(0.45);
 	leg5->SetBorderSize(0);
 	leg5->Draw();
-	if(savePlots && useCohRW) p5->SaveAs((plotsFolder+"Emupi_fgd1RW.pdf").c_str());
-	else if(savePlots) p5->SaveAs((plotsFolder+"Emupi_fgd1.pdf").c_str());
 	c1->cd();
 
 	TPad* p6=new TPad("p6","",0.5,0.0,1.0,0.32,0);
@@ -104,14 +96,12 @@ void DrawBinnings(){
 	draw.SetTitleY("Events");
 	draw.SetLegendParam(0.2,0.75,0.475,0.85);
 	double xbins6[21]={90,150,180};
-	draw.Draw(exp,"all","all","ThetaCoplanar*180/3.14",2,xbins6,"reaccoh","accum_level[][0][0]>9 "+reco_cut,"text"," NOVARBIN  WCORR");
+	draw.Draw(exp,"all","all","ThetaCoplanar*180/3.14",2,xbins6,"reaccoh","accum_level[][0][0]>9 "+reco_cut,"text"," NOVARBIN NODATA  WCORR");
 	histos[5] = draw.GetLastStackTotal();
 	TLegend * leg6 = draw.GetLastLegend();
 	leg6->SetMargin(0.45);
 	leg6->SetBorderSize(0);
 	leg6->Draw();
-	if(savePlots && useCohRW) p6->SaveAs((plotsFolder+"Theta_mupi_fgd1RW.pdf").c_str());
-	else if(savePlots) p6->SaveAs((plotsFolder+"Theta_mupi_fgd1.pdf").c_str());
 	c1->cd();
 
 	// FGD2
@@ -123,14 +113,12 @@ void DrawBinnings(){
 	draw.SetTitleY("Events");
 	draw.SetLegendParam(0.6,0.55,0.85,0.65);
 	double xbins[3]={0.2,0.7,5};
-	draw.Draw(exp,"all","all","selmu_mom/1000",2,xbins,"fgd2reaccoh","accum_level[][1][0]>9"+reco_cut,"text"," NOVARBIN  WCORR");
+	draw.Draw(exp,"all","all","selmu_mom/1000",2,xbins,"fgd2reaccoh","accum_level[][1][0]>9"+reco_cut,"text"," NOVARBIN NODATA  WCORR");
 	histos[6] = draw.GetLastStackTotal();
 	TLegend * leg1 = draw.GetLastLegend();
 	leg1->SetMargin(0.45);
 	leg1->SetBorderSize(1);
 	leg1->Draw();
-	if(savePlots && useCohRW) p1->SaveAs((plotsFolder+"mumom_fgd2RW.pdf").c_str());
-	else if(savePlots) p1->SaveAs((plotsFolder+"mumom_fgd2.pdf").c_str());
 	c2->cd();
 
 	TPad* p2=new TPad("p2","",0.5,0.67,1.0,1.0,0);
@@ -139,14 +127,12 @@ void DrawBinnings(){
 	draw.SetTitleY("Events");
 	draw.SetLegendParam(0.2,0.55,0.45,0.65);
 	double xbins2[3]={0.7,0.95,1};
-	draw.Draw(exp,"all","all","selmu_costheta",2,xbins2,"fgd2reaccoh","accum_level[][1][0]>9"+reco_cut,"text"," NOVARBIN  WCORR");
+	draw.Draw(exp,"all","all","selmu_costheta",2,xbins2,"fgd2reaccoh","accum_level[][1][0]>9"+reco_cut,"text"," NOVARBIN NODATA  WCORR");
 	histos[7] = draw.GetLastStackTotal();
 	TLegend * leg2 = draw.GetLastLegend();
 	leg2->SetMargin(0.45);
 	leg2->SetBorderSize(1);
 	leg2->Draw();
-	if(savePlots && useCohRW) p2->SaveAs((plotsFolder+"mutheta_fgd2RW.pdf").c_str());
-	else if(savePlots) p2->SaveAs((plotsFolder+"mutheta_fgd2.pdf").c_str());
 	c2->cd();
 
 	TPad* p3=new TPad("p3","",0.0,0.33,0.5,0.66,0);
@@ -155,14 +141,12 @@ void DrawBinnings(){
 	draw.SetTitleY("Events");
 	draw.SetLegendParam(0.6,0.55,0.85,0.65);
 	double xbins3[3]={0.15,0.4,1.5};
-	draw.Draw(exp,"all","all","selpi_mom/1000",2,xbins3,"fgd2reaccoh","accum_level[][1][0]>9 "+reco_cut,"text"," NOVARBIN  WCORR");
+	draw.Draw(exp,"all","all","selpi_mom/1000",2,xbins3,"fgd2reaccoh","accum_level[][1][0]>9 "+reco_cut,"text"," NOVARBIN NODATA  WCORR");
 	histos[8] = draw.GetLastStackTotal();
 	TLegend * leg3 = draw.GetLastLegend();
 	leg3->SetMargin(0.45);
 	leg3->SetBorderSize(1);
 	leg3->Draw();
-	if(savePlots && useCohRW) p3->SaveAs((plotsFolder+"pimom_fgd2RW.pdf").c_str());
-	else if(savePlots) p3->SaveAs((plotsFolder+"pimom_fgd2.pdf").c_str());
 	c2->cd();
 
 	TPad* p4=new TPad("p4","",0.5,0.33,1.0,0.66,0);
@@ -171,14 +155,12 @@ void DrawBinnings(){
 	draw.SetTitleY("Events");
 	draw.SetLegendParam(0.2,0.55,0.45,0.65);
 	double xbins4[3]={0.45,0.9,1};
-	draw.Draw(exp,"all","all","selpi_costheta",2,xbins4,"fgd2reaccoh","accum_level[][1][0]>9 "+reco_cut,"text"," NOVARBIN  WCORR");
+	draw.Draw(exp,"all","all","selpi_costheta",2,xbins4,"fgd2reaccoh","accum_level[][1][0]>9 "+reco_cut,"text"," NOVARBIN NODATA  WCORR");
 	histos[9] = draw.GetLastStackTotal();	
 	TLegend * leg4 = draw.GetLastLegend();
 	leg4->SetMargin(0.45);
 	leg4->SetBorderSize(1);
 	leg4->Draw();
-	if(savePlots && useCohRW) p4->SaveAs((plotsFolder+"pitheta_fgd2RW.pdf").c_str());
-	else if(savePlots) p4->SaveAs((plotsFolder+"pitheta_fgd2.pdf").c_str());
 	c2->cd();
 
 	TPad* p5=new TPad("p5","",0.0,0.0,0.5,0.32,0);
@@ -187,14 +169,12 @@ void DrawBinnings(){
 	draw.SetTitleY("Events");
 	draw.SetLegendParam(0.6,0.70,0.875,0.85);
 	double xbins5[3]={0.5,1.25,6.5};
-	draw.Draw(exp,"all","all","Emupi/1000.",2,xbins5,"fgd2reaccoh","accum_level[][1][0]>9 "+reco_cut,"text"," NOVARBIN  WCORR");
+	draw.Draw(exp,"all","all","Emupi/1000.",2,xbins5,"fgd2reaccoh","accum_level[][1][0]>9 "+reco_cut,"text"," NOVARBIN NODATA  WCORR");
 	histos[10] = draw.GetLastStackTotal();
 	TLegend * leg5 = draw.GetLastLegend();
 	leg5->SetMargin(0.45);
 	leg5->SetBorderSize(0);
 	leg5->Draw();
-	if(savePlots && useCohRW) p5->SaveAs((plotsFolder+"Emupi_fgd2RW.pdf").c_str());
-	else if(savePlots) p5->SaveAs((plotsFolder+"Emupi_fgd2.pdf").c_str());
 	c2->cd();
 
 	TPad* p6=new TPad("p6","",0.5,0.0,1.0,0.32,0);
@@ -203,64 +183,62 @@ void DrawBinnings(){
 	draw.SetTitleY("Events");
 	draw.SetLegendParam(0.2,0.75,0.475,0.85);
 	double xbins6[21]={90,150,180};
-	draw.Draw(exp,"all","all","ThetaCoplanar*180/3.14",2,xbins6,"fgd2reaccoh","accum_level[][1][0]>9 "+reco_cut,"text"," NOVARBIN  WCORR");
+	draw.Draw(exp,"all","all","ThetaCoplanar*180/3.14",2,xbins6,"fgd2reaccoh","accum_level[][1][0]>9 "+reco_cut,"text"," NOVARBIN NODATA  WCORR");
 	histos[11] = draw.GetLastStackTotal();
 	TLegend * leg6 = draw.GetLastLegend();
 	leg6->SetMargin(0.45);
 	leg6->SetBorderSize(0);
 	leg6->Draw();
-	if(savePlots && useCohRW) p6->SaveAs((plotsFolder+"Theta_mupi_fgd2RW.pdf").c_str());
-	else if(savePlots) p6->SaveAs((plotsFolder+"Theta_mupi_fgd2.pdf").c_str());
 	c2->cd();
 
-	draw.Draw(exp,"all","all","selmu_mom/1000",2,xbins,"reaction","accum_level[][0][0]>9 && reaction==3"+reco_cut,"text"," NOVARBIN  WCORR NODRAW");
+	draw.Draw(exp,"all","all","selmu_mom/1000",2,xbins,"reaction","accum_level[][0][0]>9 && reaction==3"+reco_cut,"text"," NOVARBIN NODATA  WCORR NODRAW");
 	histos[12] = draw.GetLastStackTotal();
-	draw.Draw(exp,"all","all","selmu_costheta",2,xbins2,"reaction","accum_level[][0][0]>9 && reaction==3"+reco_cut,"text"," NOVARBIN NODRAW WCORR");
+	draw.Draw(exp,"all","all","selmu_costheta",2,xbins2,"reaction","accum_level[][0][0]>9 && reaction==3"+reco_cut,"text"," NOVARBIN NODATA NODRAW WCORR");
 	histos[13] = draw.GetLastStackTotal();
-	draw.Draw(exp,"all","all","selpi_mom/1000",2,xbins3,"reaction","accum_level[][0][0]>9 && reaction==3"+reco_cut,"text"," NOVARBIN NODRAW WCORR");
+	draw.Draw(exp,"all","all","selpi_mom/1000",2,xbins3,"reaction","accum_level[][0][0]>9 && reaction==3"+reco_cut,"text"," NOVARBIN NODATA NODRAW WCORR");
 	histos[14] = draw.GetLastStackTotal();
-	draw.Draw(exp,"all","all","selpi_costheta",2,xbins4,"reaction","accum_level[][0][0]>9 && reaction==3"+reco_cut,"text"," NOVARBIN NODRAW WCORR");
+	draw.Draw(exp,"all","all","selpi_costheta",2,xbins4,"reaction","accum_level[][0][0]>9 && reaction==3"+reco_cut,"text"," NOVARBIN NODATA NODRAW WCORR");
 	histos[15] = draw.GetLastStackTotal();
-	draw.Draw(exp,"all","all","Emupi/1000.",2,xbins5,"reaction","accum_level[][0][0]>9 && reaction==3"+reco_cut,"text"," NOVARBIN NODRAW WCORR");
+	draw.Draw(exp,"all","all","Emupi/1000.",2,xbins5,"reaction","accum_level[][0][0]>9 && reaction==3"+reco_cut,"text"," NOVARBIN NODATA NODRAW WCORR");
 	histos[16] = draw.GetLastStackTotal();
-	draw.Draw(exp,"all","all","ThetaCoplanar*180/3.14",2,xbins6,"reaction","accum_level[][0][0]>9 && reaction==3 "+reco_cut,"text"," NOVARBIN NODRAW WCORR");
+	draw.Draw(exp,"all","all","ThetaCoplanar*180/3.14",2,xbins6,"reaction","accum_level[][0][0]>9 && reaction==3 "+reco_cut,"text"," NOVARBIN NODATA NODRAW WCORR");
 	histos[17] = draw.GetLastStackTotal();
-	draw.Draw(exp,"all","all","selmu_mom/1000",2,xbins,"fgd2reaction","accum_level[][1][0]>9 && fgd2reaction==3"+reco_cut,"text"," NODRAW NOVARBIN  WCORR");
+	draw.Draw(exp,"all","all","selmu_mom/1000",2,xbins,"fgd2reaction","accum_level[][1][0]>9 && fgd2reaction==3"+reco_cut,"text"," NODRAW NOVARBIN NODATA  WCORR");
 	histos[18] = draw.GetLastStackTotal();
-	draw.Draw(exp,"all","all","selmu_costheta",2,xbins2,"fgd2reaction","accum_level[][1][0]>9 && fgd2reaction==3 "+reco_cut,"text"," NODRAW NOVARBIN  WCORR");
+	draw.Draw(exp,"all","all","selmu_costheta",2,xbins2,"fgd2reaction","accum_level[][1][0]>9 && fgd2reaction==3 "+reco_cut,"text"," NODRAW NOVARBIN NODATA  WCORR");
 	histos[19] = draw.GetLastStackTotal();
-	draw.Draw(exp,"all","all","selpi_mom/1000",2,xbins3,"fgd2reaction","accum_level[][1][0]>9 && fgd2reaction==3 "+reco_cut,"text"," NODRAW NOVARBIN  WCORR");
+	draw.Draw(exp,"all","all","selpi_mom/1000",2,xbins3,"fgd2reaction","accum_level[][1][0]>9 && fgd2reaction==3 "+reco_cut,"text"," NODRAW NOVARBIN NODATA  WCORR");
 	histos[20] = draw.GetLastStackTotal();
-	draw.Draw(exp,"all","all","selpi_costheta",2,xbins4,"fgd2reaction","accum_level[][1][0]>9 && fgd2reaction==3"+reco_cut,"text"," NODRAW NOVARBIN  WCORR");
+	draw.Draw(exp,"all","all","selpi_costheta",2,xbins4,"fgd2reaction","accum_level[][1][0]>9 && fgd2reaction==3"+reco_cut,"text"," NODRAW NOVARBIN NODATA  WCORR");
 	histos[21] = draw.GetLastStackTotal();
-	draw.Draw(exp,"all","all","Emupi/1000.",2,xbins5,"fgd2reaction","accum_level[][1][0]>9 && fgd2reaction==3"+reco_cut,"text"," NODRAW NOVARBIN  WCORR");
+	draw.Draw(exp,"all","all","Emupi/1000.",2,xbins5,"fgd2reaction","accum_level[][1][0]>9 && fgd2reaction==3"+reco_cut,"text"," NODRAW NOVARBIN NODATA  WCORR");
 	histos[22] = draw.GetLastStackTotal();
-	draw.Draw(exp,"all","all","ThetaCoplanar*180/3.14",2,xbins6,"fgd2reaction","accum_level[][1][0]>9 && fgd2reaction==3 "+reco_cut,"text"," NODRAW NOVARBIN  WCORR");
+	draw.Draw(exp,"all","all","ThetaCoplanar*180/3.14",2,xbins6,"fgd2reaction","accum_level[][1][0]>9 && fgd2reaction==3 "+reco_cut,"text"," NODRAW NOVARBIN NODATA  WCORR");
 	histos[23] = draw.GetLastStackTotal();
 
-	draw.Draw(exp,"all","all","selmu_mom/1000",2,xbins,"reaccoh","accum_level[][0][0]>9 && reaccoh==3"+reco_cut,"text"," NOVARBIN  WCORR NODRAW");
+	draw.Draw(exp,"all","all","selmu_mom/1000",2,xbins,"reaccoh","accum_level[][0][0]>9 && reaccoh==3"+reco_cut,"text"," NOVARBIN NODATA  WCORR NODRAW");
 	histos[24] = draw.GetLastStackTotal();
-	draw.Draw(exp,"all","all","selmu_costheta",2,xbins2,"reaccoh","accum_level[][0][0]>9 && reaccoh==3"+reco_cut,"text"," NOVARBIN NODRAW WCORR");
+	draw.Draw(exp,"all","all","selmu_costheta",2,xbins2,"reaccoh","accum_level[][0][0]>9 && reaccoh==3"+reco_cut,"text"," NOVARBIN NODATA NODRAW WCORR");
 	histos[25] = draw.GetLastStackTotal();
-	draw.Draw(exp,"all","all","selpi_mom/1000",2,xbins3,"reaccoh","accum_level[][0][0]>9 && reaccoh==3"+reco_cut,"text"," NOVARBIN NODRAW WCORR");
+	draw.Draw(exp,"all","all","selpi_mom/1000",2,xbins3,"reaccoh","accum_level[][0][0]>9 && reaccoh==3"+reco_cut,"text"," NOVARBIN NODATA NODRAW WCORR");
 	histos[26] = draw.GetLastStackTotal();
-	draw.Draw(exp,"all","all","selpi_costheta",2,xbins4,"reaccoh","accum_level[][0][0]>9 && reaccoh==3"+reco_cut,"text"," NOVARBIN NODRAW WCORR");
+	draw.Draw(exp,"all","all","selpi_costheta",2,xbins4,"reaccoh","accum_level[][0][0]>9 && reaccoh==3"+reco_cut,"text"," NOVARBIN NODATA NODRAW WCORR");
 	histos[27] = draw.GetLastStackTotal();
-	draw.Draw(exp,"all","all","Emupi/1000.",2,xbins5,"reaccoh","accum_level[][0][0]>9 && reaccoh==3"+reco_cut,"text"," NOVARBIN NODRAW WCORR");
+	draw.Draw(exp,"all","all","Emupi/1000.",2,xbins5,"reaccoh","accum_level[][0][0]>9 && reaccoh==3"+reco_cut,"text"," NOVARBIN NODATA NODRAW WCORR");
 	histos[28] = draw.GetLastStackTotal();
-	draw.Draw(exp,"all","all","ThetaCoplanar*180/3.14",2,xbins6,"reaccoh","accum_level[][0][0]>9 && reaccoh==3 "+reco_cut,"text"," NOVARBIN NODRAW WCORR");
+	draw.Draw(exp,"all","all","ThetaCoplanar*180/3.14",2,xbins6,"reaccoh","accum_level[][0][0]>9 && reaccoh==3 "+reco_cut,"text"," NOVARBIN NODATA NODRAW WCORR");
 	histos[29] = draw.GetLastStackTotal();
-	draw.Draw(exp,"all","all","selmu_mom/1000",2,xbins,"fgd2reaccoh","accum_level[][1][0]>9 && fgd2reaccoh==3"+reco_cut,"text"," NODRAW NOVARBIN  WCORR");
+	draw.Draw(exp,"all","all","selmu_mom/1000",2,xbins,"fgd2reaccoh","accum_level[][1][0]>9 && fgd2reaccoh==3"+reco_cut,"text"," NODRAW NOVARBIN NODATA  WCORR");
 	histos[30] = draw.GetLastStackTotal();
-	draw.Draw(exp,"all","all","selmu_costheta",2,xbins2,"fgd2reaccoh","accum_level[][1][0]>9 && fgd2reaccoh==3 "+reco_cut,"text"," NODRAW NOVARBIN  WCORR");
+	draw.Draw(exp,"all","all","selmu_costheta",2,xbins2,"fgd2reaccoh","accum_level[][1][0]>9 && fgd2reaccoh==3 "+reco_cut,"text"," NODRAW NOVARBIN NODATA  WCORR");
 	histos[31] = draw.GetLastStackTotal();
-	draw.Draw(exp,"all","all","selpi_mom/1000",2,xbins3,"fgd2reaccoh","accum_level[][1][0]>9 && fgd2reaccoh==3 "+reco_cut,"text"," NODRAW NOVARBIN  WCORR");
+	draw.Draw(exp,"all","all","selpi_mom/1000",2,xbins3,"fgd2reaccoh","accum_level[][1][0]>9 && fgd2reaccoh==3 "+reco_cut,"text"," NODRAW NOVARBIN NODATA  WCORR");
 	histos[32] = draw.GetLastStackTotal();
-	draw.Draw(exp,"all","all","selpi_costheta",2,xbins4,"fgd2reaccoh","accum_level[][1][0]>9 && fgd2reaccoh==3"+reco_cut,"text"," NODRAW NOVARBIN  WCORR");
+	draw.Draw(exp,"all","all","selpi_costheta",2,xbins4,"fgd2reaccoh","accum_level[][1][0]>9 && fgd2reaccoh==3"+reco_cut,"text"," NODRAW NOVARBIN NODATA  WCORR");
 	histos[33] = draw.GetLastStackTotal();
-	draw.Draw(exp,"all","all","Emupi/1000.",2,xbins5,"fgd2reaccoh","accum_level[][1][0]>9 && fgd2reaccoh==3"+reco_cut,"text"," NODRAW NOVARBIN  WCORR");
+	draw.Draw(exp,"all","all","Emupi/1000.",2,xbins5,"fgd2reaccoh","accum_level[][1][0]>9 && fgd2reaccoh==3"+reco_cut,"text"," NODRAW NOVARBIN NODATA  WCORR");
 	histos[34] = draw.GetLastStackTotal();
-	draw.Draw(exp,"all","all","ThetaCoplanar*180/3.14",2,xbins6,"fgd2reaccoh","accum_level[][1][0]>9 && fgd2reaccoh==3 "+reco_cut,"text"," NODRAW NOVARBIN  WCORR");
+	draw.Draw(exp,"all","all","ThetaCoplanar*180/3.14",2,xbins6,"fgd2reaccoh","accum_level[][1][0]>9 && fgd2reaccoh==3 "+reco_cut,"text"," NODRAW NOVARBIN NODATA  WCORR");
 	histos[35] = draw.GetLastStackTotal();
 
 	TCanvas *c3 = new TCanvas("c3","c3",900,600);
